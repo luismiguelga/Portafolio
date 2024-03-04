5039f86734f58972d6a67e11a2c2b42168802a11
Luis Miguel <luismigalvis99@gmail.com>
2024-03-04 18:57:13 -0500

    Cambios rama

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,4 +1,40 @@
 <template>
-    <h1>Hola soy el new home</h1>
-    <div class="info1"></div>
+    <div class="og">
+        <div class="info1">
+            <h1>Soy info1</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, temporibus doloremque. Tempore hic nisi facere dolor enim inventore voluptas at vel. Saepe, distinctio repellat asperiores quae ex quaerat facere inventore!</p>
+        </div>
+        <div class="info2">
+            <h1>Soy info2</h1>
+        </div>
+    </div>
 </template>
+
+
+<style>
+body {
+    display: flex;
+    justify-content: center;
+    margin: 0px;
+}
+
+.info1 {
+    background-color: white;
+    height: 600px;
+    width: 550px;
+    border-radius: 20px;
+}
+.og{
+    display: flex;
+    gap: 30px;
+    margin-top: 50px;
+}
+
+.info2 {
+    background-color: white;
+    height: 600px;
+    width: 550px;
+    border-radius: 20px;
+    display: flex;
+}
+</style>
