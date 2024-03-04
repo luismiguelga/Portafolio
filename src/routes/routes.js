f526e10fb68bb807827af08aa576cd6709669918
Luis Miguel <luismigalvis99@gmail.com>
2024-03-04 09:50:01 -0500

    Portafolio v2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from "vue-router"
 import home from "../components/home.vue"
 import tienda from "../components/tienda/tienda.vue"
 import pokedex from "../components/pokedex/pokedex.vue"
+import Portafolio from "../components/Portafolio.vue"
 
 const routes =[
-  { path: "/", component: home},
-  { path:"/tienda", component: tienda},
-  {path: "/pokedex", component: pokedex}
+  {path: "/", component: home},
+  {path: "/tienda", component: tienda},
+  {path: "/pokedex", component: pokedex},
+  {path: "/Portafolio", component: Portafolio}
 ]
 
 export const router = createRouter({
